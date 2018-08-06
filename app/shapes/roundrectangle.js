91e5fac4f74edefd91413916c75b7a1e73510a55
sergei_iliev <sergei.iliev@gmail.com>
2018-08-06 16:37:29 +0300

    round

--- app/shapes/roundrectangle.js
+++ app/shapes/roundrectangle.js
@@ -1,14 +1,18 @@
 module.exports = function(d2) {
-
+    /*
+     * Round rectangle is represented by 4 segments and 4 arcs
+     */
     d2.RoundRectangle = class RoundRectangle {
     	constructor(p1,width,height,rounding) {
     		this.pl=pl;
     		this.width=width;
     		this.height=height;
     		this.rounding=rounding;
-    		
+    		this.init();
     	}
-    	
+    	init(){
+    	      	
+    	}
     	rotate(angle,center = {x:0, y:0}){
     		
     	}
